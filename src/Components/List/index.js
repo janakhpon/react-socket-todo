854dcc604e9858169cb57cb469f6ecb6fb9f1b8a
janakhpon <jnovaxer@gmail.com>
2020-01-11 15:09:47 +0630

    GDEV0003

--- src/Components/List/index.js
+++ src/Components/List/index.js
@@ -1,25 +1,99 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Table } from 'semantic-ui-react'
 import PageListItem from '../ListItem'
+import { Button, Header, Icon, Modal, Input } from 'semantic-ui-react'
+import axios from 'axios'
+import { MAIN_URL, URL_ID } from '../../Const'
 
-const PageList = () => (
-    <>
-        <Table celled inverted selectable>
-            <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell>DATE</Table.HeaderCell>
-                    <Table.HeaderCell>TASK</Table.HeaderCell>
-                    <Table.HeaderCell>CREATE</Table.HeaderCell>
-                    <Table.HeaderCell>UPDATE</Table.HeaderCell>
-                    <Table.HeaderCell>DELETE</Table.HeaderCell>
-                </Table.Row>
-            </Table.Header>
+const PageList = () => {
+    const [values, setValues] = React.useState([])
+    const [text, setText] = React.useState("")
 
-            <Table.Body>
-                <PageListItem />
-            </Table.Body>
-        </Table>
-    </>
-)
+    useEffect(() => {
+        let isSubscribed = true
+        const getUser = async () => {
+            try {
+                let cb = await axios.get(MAIN_URL)
+
+                if (isSubscribed) {
+                    console.log(cb.data)
+                    setValues(cb.data)
+                }
+            } catch (err) {
+
+            }
+        }
+        try {
+            getUser()
+        } catch (err) {
+
+        }
+        return () => isSubscribed = false
+    }, [])
+
+    const onChange = (e) => {
+        setText(e.target.value)
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        let data = {
+            text: text
+        }
+        console.log(MAIN_URL)
+        try {
+            let response = await axios({
+                method: 'post',
+                url: MAIN_URL,
+                data: data,
+                config: { headers: { 'Content-Type': 'application/json' } }
+            })
+
+            console.log(response)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+
+
+    return (
+        <>
+            <Table celled inverted selectable>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>DATE</Table.HeaderCell>
+                        <Table.HeaderCell>TASK</Table.HeaderCell>
+                        <Table.HeaderCell textAlign='center'>
+                            <Modal trigger={<Button primary>CREATE</Button>} basic size='small'>
+                                <Header icon='archive' content='Create your task here' />
+                                <Modal.Content>
+                                    <Input focus placeholder='Your text' value={text} name="text" onChange={onChange} />
+                                </Modal.Content>
+                                <Modal.Actions>
+                                    <Button basic color='red' inverted>
+                                        <Icon name='remove' /> No
+                                </Button>
+                                    <Button color='green' inverted onClick={onSubmit}>
+                                        <Icon name='checkmark' /> SAVE
+                                </Button>
+                                </Modal.Actions>
+                            </Modal>
+                        </Table.HeaderCell>
+                        <Table.HeaderCell textAlign='center'>OPTIONS</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    {
+                        values.map((val, key) => {
+                            return <PageListItem item={val} key={key} />
+                        })
+                    }
+                </Table.Body>
+            </Table>
+        </>
+    )
+}
 
 export default PageList
--- src/Components/ListItem/index.js
+++ src/Components/ListItem/index.js
@@ -1,29 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Table } from 'semantic-ui-react'
 import { Button, Header, Icon, Modal, Input } from 'semantic-ui-react'
 import axios from 'axios'
-import { MAIN_URL } from '../../Const'
+import { URL_ID } from '../../Const'
 
-const PageListItem = () => {
-    const [text, setText] = React.useState("")
-    const formData = new FormData()
+// const INIT_STATE = {
+//     _id: "",
+//     date: "",
+//     text: ""
+// }
+const PageListItem = ({ item, key }) => {
+
+    const { _id, text, date } = item
+    const key_val = key
+
+    const INIT_STATE = {
+        text: text
+    }
+    const [ textval, setTextval ] = React.useState(INIT_STATE)
+
+
 
 
     const onChange = (e) => {
-        setText(e.target.value)
-        formData.set("text", e.target.value)
+        setTextval({ text: e.target.value })
     }
 
-    const onSubmit = async (e) => {
+
+    const onUpdate = async (e) => {
         e.preventDefault()
+        let url = `${URL_ID}${_id}`
         let data = {
-            text: text
+            text: textval.text
         }
-        console.log(MAIN_URL)
+
         try {
             let response = await axios({
-                method: 'post',
-                url: MAIN_URL,
+                method: 'put',
+                url: url,
                 data: data,
                 config: { headers: { 'Content-Type': 'application/json' } }
             })
@@ -34,35 +48,48 @@ const PageListItem = () => {
         }
     }
 
+    const onDelete = async (e) => {
+        e.preventDefault()
+        let url = `${URL_ID}${_id}`
+
+        try {
+            let response = await axios({
+                method: 'delete',
+                url: url,
+                config: { headers: { 'Content-Type': 'application/json' } }
+            })
+
+            console.log(response)
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
+
     return (
-        <>
-            <Table.Row>
-                <Table.Cell>John</Table.Cell>
-                <Table.Cell>Approved</Table.Cell>
-                <Table.Cell textAlign='center'>
-                    <Modal trigger={<Button primary>CREATE</Button>} basic size='small'>
-                        <Header icon='archive' content='Archive Old Messages' />
-                        <Modal.Content>
-                            <Input focus placeholder='Your text' value={text} name="text" onChange={onChange} />
-                        </Modal.Content>
-                        <Modal.Actions>
-                            <Button basic color='red' inverted>
-                                <Icon name='remove' /> No
-                            </Button>
-                            <Button color='green' inverted onClick={onSubmit}>
-                                <Icon name='checkmark' /> SAVE
-                            </Button>
-                        </Modal.Actions>
-                    </Modal>
-                </Table.Cell>
-                <Table.Cell textAlign='center'>
-                    <Button primary>UPDATE</Button>
-                </Table.Cell>
-                <Table.Cell textAlign='center'>
-                    <Button primary>DELETE</Button>
-                </Table.Cell>
-            </Table.Row>
-        </>
+        <Table.Row key={key_val}>
+            <Table.Cell>{date}</Table.Cell>
+            <Table.Cell>{text}</Table.Cell>
+            <Table.Cell textAlign='center'>
+                <Modal trigger={<Button primary>UPDATE</Button>} basic size='small'>
+                    <Header icon='archive' content='Create your task' />
+                    <Modal.Content>
+                        <Input focus  value={textval.text} name="text" onChange={onChange} />
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button basic color='red' inverted>
+                            <Icon name='remove' /> No
+                        </Button>
+                        <Button color='green' inverted onClick={onUpdate}>
+                            <Icon name='checkmark' /> SAVE
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
+            </Table.Cell>
+            <Table.Cell textAlign='center'>
+                <Button primary onClick={onDelete}>DELETE</Button>
+            </Table.Cell>
+        </Table.Row>
     )
 }
 
